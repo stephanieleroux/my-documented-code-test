141f83542af06de4fd92cadb39e72c8201e07d2c
Stephanie Leroux <leroux.ste@gmail.com>
2022-09-09 21:39:44 +0200

    SLX: fix

--- docs/conf.py
+++ docs/conf.py
@@ -58,7 +58,7 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 #html_theme = 'classic'
-html_theme = 'sphinx_rtd_theme'
+#html_theme = 'sphinx_rtd_theme'
 
 
 # Add any paths that contain custom static files (such as style sheets) here,
