a3f144c4ee6bbea1298aab14647d34e0e76deefa
Stephanie Leroux <leroux.ste@gmail.com>
2022-09-09 16:33:02 +0200

    SLX: bugged

--- docs/conf.py
+++ docs/conf.py
@@ -12,10 +12,11 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../my_dumb_math_tools/'))
+#sys.path.insert(0, os.path.abspath('../my_dumb_math_tools/'))
+sys.path.insert(0, os.path.abspath('../'))
 
-autodoc_mock_imports = ['numpy']
-autodoc_mock_imports = ['random']
+#autodoc_mock_imports = ['numpy']
+#autodoc_mock_imports = ['random']
 
 # -- Project information -----------------------------------------------------
 
@@ -34,9 +35,12 @@ master_doc = 'index'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.napoleon'
+    'sphinx.ext.autodoc'
 ]
-napoleon_google_docstring = False
+#extensions = [
+#    'sphinx.ext.napoleon'
+#]
+#napoleon_google_docstring = False
 
 
 
@@ -61,5 +65,5 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+#html_static_path = ['_static']
 
