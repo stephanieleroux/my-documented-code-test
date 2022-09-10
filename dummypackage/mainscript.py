e2704a363f1db60c5886f9590c72df3e1640100d
Stephanie Leroux <leroux.ste@gmail.com>
2022-09-10 07:59:45 +0200

    slx:fix

---
+++ dummypackage/mainscript.py
@@ -0,0 +1,11 @@
+"""Main script
+This is my main script. It calls a dummy function function from  my library of (dummy) tools in srcpy/libtools.py
+"""
+
+from srcpy import libtools as lt
+
+print('hello')
+
+
+print(lt.mul(3,9))
+
